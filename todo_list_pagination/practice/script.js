d9a791aeb2ea8c7cb05b9a033a4403be04eb0914
ssub0227 <sb010227@gmail.com>
2023-11-02 03:41:50 +0900

    feat: pagenation 기능 추가

--- todo_list_pagination/practice/script.js
+++ todo_list_pagination/practice/script.js
@@ -4,16 +4,70 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+  const getAll = (target) => {
+    return document.querySelectorAll(target)
+  }
 
-  const API_URL = 'http://localhost:3000/todos';
-  const $todos = get('.todos');
-  const $form = get('.todo_form');
-  const $todoInput = get('.todo_input');
+  const $todos = get('.todos')
+  const $form = get('.todo_form')
+  const $todoInput = get('.todo_input')
+  const $pagination = get('.pagination')
+  const API_URL = `http://localhost:3000/todos`
+
+  let currentPage = 1
+  const totalCount = 53
+  const pageCount = 5
+  const limit = 5
+
+  const pagination = () => {
+    let totalPage = Math.ceil(totalCount / limit)
+    let pageGroup = Math.ceil(currentPage / pageCount)
+    let lastNumber = pageGroup * pageCount
+    if (lastNumber > totalPage) {
+      lastNumber = totalPage
+    }
+    let firstNumber = lastNumber - (pageCount - 1)
+
+    const next = lastNumber + 1
+    const prev = firstNumber - 1
+
+    let html = ''
+
+    if (prev > 0) {
+      html += "<button class='prev' data-fn='prev'>이전</button> "
+    }
+
+    for (let i = firstNumber; i <= lastNumber; i++) {
+      html += `<button class="pageNumber" id="page_${i}">${i}</button>`
+    }
+    if (lastNumber < totalPage) {
+      html += `<button class='next' data-fn='next'>다음</button>`
+    }
+
+    $pagination.innerHTML = html
+    const $currentPageNumber = get(`.pageNumber#page_${currentPage}`)
+    $currentPageNumber.style.color = '#9dc0e8'
+
+    const $currentPageNumbers = getAll(`.pagination button`)
+    $currentPageNumbers.forEach((button) => {
+      button.addEventListener('click', () => {
+        if (button.dataset.fn === 'prev') {
+          currentPage = prev
+        } else if (button.dataset.fn === 'next') {
+          currentPage = next
+        } else {
+          currentPage = button.innerText
+        }
+        pagination()
+        getTodos()
+      })
+    })
+  }
 
   const createTodoElement = (item) => {
     const { id, content, completed } = item
+    const isChecked = completed ? 'checked' : ''
     const $todoItem = document.createElement('div')
-    const isChecked = completed == true ? "checked" : ''
     $todoItem.classList.add('item')
     $todoItem.dataset.id = id
     $todoItem.innerHTML = `
@@ -46,115 +100,129 @@
     return $todoItem
   }
 
-  const renderAllTodos = (todos) =>{
-    $todos.innerHTML ='';
-    todos.forEach(item => {
-      const todoElement = createTodoElement(item);
-      $todos.appendChild(todoElement);
-    });
+  const renderAllTodos = (todos) => {
+    $todos.innerHTML = ''
+    todos.forEach((item) => {
+      const todoElement = createTodoElement(item)
+      $todos.appendChild(todoElement)
+    })
   }
 
-  // json-server fetch
-  const getTodos = () =>{
-    fetch(API_URL).then((response) => response.json()).then((response) => renderAllTodos(response)).catch((error) => console.error(error))
-  } 
+  const getTodos = () => {
+    fetch(`${API_URL}?_page=${currentPage}&_limit=${limit}`)
+      .then((response) => response.json())
+      .then((todos) => {
+        renderAllTodos(todos)
+      })
+      .catch((error) => console.error(error.message))
+  }
 
-  // input 입력시 list에 add
   const addTodo = (e) => {
-    e.preventDefault(); // sumbit 이벤트의 경우 제출시 새로고침이 기본이라 그 기능을 막음. 
+    e.preventDefault()
+    const content = $todoInput.value
+    if (!content) return
     const todo = {
-     content: $todoInput.value,
-     completed:false,   
+      content,
+      completed: false,
     }
-    fetch(API_URL,{
-      method:'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(todo), // javascript 객체를 json 으로 변환
-    }).then(getTodos).then(() => {$todoInput.value = '', $todoInput.focus()}).catch((error) => console.error(error))
+    fetch(API_URL, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(todo),
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .then(() => {
+        $todoInput.value = ''
+        $todoInput.focus()
+      })
+      .catch((error) => console.error(error.message))
   }
 
-  //체크박스 통신
   const toggleTodo = (e) => {
     if (e.target.className !== 'todo_checkbox') return
-    const completed = e.target.checked;
-    const $item = e.target.closest('.item');
-    const id = $item.dataset.id;
-    
-    fetch(`${API_URL}/${id}`,{
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
+    const completed = e.target.checked
+    fetch(`${API_URL}/${id}`, {
       method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({ completed }),
-    }).then(getTodos).catch((error)=>{console.error(error)})
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .catch((error) => console.error(error.message))
   }
 
-  // 수정 버튼 눌렀을 경우 
-  const changeEditMode = (e) =>{
-    const $item = e.target.closest('.item');
-    const $label = $item.querySelector('label');
-    const $editInput = $item.querySelector('input[type="text"]');
-    const $contentButton = $item.querySelector('.content_buttons');
-    const $editButton = $item.querySelector('.edit_buttons');   
-    const editInputValue = $editInput.value;
-    
-    if(e.target.className === 'todo_edit_button'){
-      $label.style.display ='none';
-      $editInput.style.display ='block';
-      $contentButton.style.display ='none';
-      $editButton.style.display ='flex';
-      $editInput.focus();
-      $editInput.value = '';
-      $editInput.value = editInputValue;
+  const changeEditMode = (e) => {
+    const $item = e.target.closest('.item')
+    const $label = $item.querySelector('label')
+    const $editInput = $item.querySelector('input[type="text"]')
+    const $contentButtons = $item.querySelector('.content_buttons')
+    const $editButtons = $item.querySelector('.edit_buttons')
+    const value = $editInput.value
 
+    if (e.target.className === 'todo_edit_button') {
+      $label.style.display = 'none'
+      $editInput.style.display = 'block'
+      $contentButtons.style.display = 'none'
+      $editButtons.style.display = 'block'
+      $editInput.focus()
+      $editInput.value = ''
+      $editInput.value = value
     }
 
-    if(e.target.className === 'todo_edit_cancel_button'){
-      $label.style.display ='block';
-      $editInput.style.display ='none';
-      $contentButton.style.display ='flex';
-      $editButton.style.display ='none';
-      $editInput.value = $label.innerHTML;
+    if (e.target.className === 'todo_edit_cancel_button') {
+      $label.style.display = 'block'
+      $editInput.style.display = 'none'
+      $contentButtons.style.display = 'block'
+      $editButtons.style.display = 'none'
+      $editInput.value = $label.innerText
     }
   }
 
-  // 수정 버튼 클릭 후 수정 완료 시
   const editTodo = (e) => {
-    if(e.target.classList != 'todo_edit_confirm_button') return;
-    const $item = e.target.closest('.item');
-    const id = $item.dataset.id;
-    const $editInput = $item.querySelector('input[type="text"]');
-    const content = $editInput.value;
-    
-    fetch(`${API_URL}/${id}`,{
-      method:'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ content })
-    }).then(getTodos).catch((error) => {console.error(error)})
+    if (e.target.className !== 'todo_edit_confirm_button') return
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
+    const $editInput = $item.querySelector('input[type="text"]')
+    const content = $editInput.value
+
+    fetch(`${API_URL}/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ content }),
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .catch((error) => console.error(error.message))
   }
 
-  // 투두 리스트 삭제
-  const removeTodo = (e) =>{
-    if(e.target.classList == 'todo_remove_button'){
-      const $item = e.target.closest('.item');
-      const id = $item.dataset.id; 
+  const removeTodo = (e) => {
+    if (e.target.className !== 'todo_remove_button') return
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
 
-      fetch(`${API_URL}/${id}`,{
-        method:'DELETE'}).then(getTodos).catch((error) => console.error(error))
-    }
+    fetch(`${API_URL}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .catch((error) => console.error(error.message))
   }
 
-  //실제 실행되는 함수들
   const init = () => {
-    window.addEventListener('DOMContentLoaded', ()=>{
-      getTodos();
-    });
+    window.addEventListener('DOMContentLoaded', () => {
+      getTodos()
+      pagination()
+    })
 
-    $form.addEventListener('submit', addTodo);
-    $todos.addEventListener('click',toggleTodo);
-    $todos.addEventListener('click',changeEditMode);
-    $todos.addEventListener('click',editTodo);
-    $todos.addEventListener('click',removeTodo);
+    $form.addEventListener('submit', addTodo)
+    $todos.addEventListener('click', toggleTodo)
+    $todos.addEventListener('click', changeEditMode)
+    $todos.addEventListener('click', editTodo)
+    $todos.addEventListener('click', removeTodo)
   }
+
   init()
 })()
