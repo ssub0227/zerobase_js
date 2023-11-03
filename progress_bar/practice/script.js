0b45808f48cafc2319fc423e671d1ec038aeeebb
ssub0227 <sb010227@gmail.com>
2023-11-03 13:47:24 +0900

    feat: trottle 100ms 추가

--- progress_bar/practice/script.js
+++ progress_bar/practice/script.js
@@ -1,18 +1,29 @@
-;(function () {
+;
+(function () {
   'use strict'
 
   const get = (target) => {
     return document.querySelector(target)
   }
 
+  let timerId
+  const trottle = (callback, time) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
+
   const $progressBar = get('.progress-bar')
-  const onscroll = () =>{
+
+  const onscroll = () => {
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
     const scrollTop = document.documentElement.scrollTop
 
     const width = (scrollTop / height) * 100
 
-    $progressBar.style.width = width+'%'
+    $progressBar.style.width = width + '%'
   }
-  window.addEventListener('scroll', () => onscroll())
+  window.addEventListener('scroll', () => trottle(onscroll, 100))
 })()
