9ab77200193f34372210eb7d3e095a6267b9a98d
ssub0227 <sb010227@gmail.com>
2023-11-03 13:41:54 +0900

    feat: 프로그레스 바 기능 생성

--- progress_bar/practice/script.js
+++ progress_bar/practice/script.js
@@ -4,4 +4,15 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $progressBar = get('.progress-bar')
+  const onscroll = () =>{
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    const scrollTop = document.documentElement.scrollTop
+
+    const width = (scrollTop / height) * 100
+
+    $progressBar.style.width = width+'%'
+  }
+  window.addEventListener('scroll', () => onscroll())
 })()
