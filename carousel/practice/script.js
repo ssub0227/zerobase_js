cf934a9cec449d43f75f0bb3ac3bcbd413b6b03d
ssub0227 <sb010227@gmail.com>
2023-11-14 00:15:33 +0900

    feat: carousel 움직이는 시간 움직임 막기

--- carousel/practice/script.js
+++ carousel/practice/script.js
@@ -12,6 +12,7 @@
       this.items = this.carouselElement.querySelectorAll('.carousel_item') // .carousel 밑에 있는 .carousel_item 를 items 로 연결 -> 유사배열객체로 넘어옴
       this.totalItems = this.items.length
       this.current = 0
+      this.isMoving = false
     }
 
     setEventListener(){
@@ -27,13 +28,23 @@
       })
     }
 
+    disabledInteraction(){
+      this.isMoving = true
+      setTimeout(() => {
+        this.isMoving = false
+      },500)
+    }
+
     initCarousel(){
+      this.isMoving = false
       this.items[0].classList.add('active')
       this.items[1].classList.add('next')
       this.items[this.totalItems -1 ].classList.add('prev')
     }
      
     moveCarouselTo(){
+      if(this.isMoving) return
+      this.disabledInteraction()
       let prev = this.current - 1
       let next = this.current + 1
 
@@ -57,6 +68,7 @@
     }
 
     movePrev(){
+      if(this.isMoving) return
       if(this.current === 0){
         this.current = this.totalItems - 1  //current 가 0일 경우 마지막 숫자로 변경
       } else {
@@ -66,6 +78,7 @@
     }
 
     moveNext(){
+      if(this.isMoving) return
       if(this.current === this.totalItems - 1){
         this.current = 0  //current 가 마지막일 경우에 moveNext() 호출시 current 를 0으로 변경
       } else {
@@ -75,7 +88,6 @@
     }
   }
 
-   
   document.addEventListener('DOMContentLoaded', () => {
     const carouselElement = get('.carousel')
     const carousel = new Carousel(carouselElement)
