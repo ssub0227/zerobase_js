21842529fe4d1944558271e94f6b9d4d2d109fc6
ssub0227 <sb010227@gmail.com>
2023-11-14 23:50:44 +0900

    feat: 검색 기능 추가

--- youtube/practice/script.js
+++ youtube/practice/script.js
@@ -1,3 +1,31 @@
 ;(function () {
   'use strict'
+
+  const get = (target) => document.querySelector(target)
+  const getAll = (target) => document.querySelectorAll(target)
+
+  const $search = get('#search')
+  const $list = getAll('.contents.list figure')
+  const $searchButton = get('.btn_search')
+  
+  const init = () => {
+    $search.addEventListener('keyup', search)
+    $searchButton.addEventListener('click', search)
+  }
+
+  const search = () =>{
+    let searchText = $search.value.toLowerCase()
+    for(let index = 0; index < $list.length; index ++){
+      const $target = $list[index].querySelector('strong')
+      const text = $target.textContent.toLowerCase()
+
+      if(-1 < text.indexOf(searchText)) { // 제목에 검색한 내용이 있다면 그 글자의 index가 출력됨 -> 0부터 시작
+        $list[index].style.display = 'flex'
+      } else {
+        $list[index].style.display = 'none'
+      }
+    }
+  }
+
+  init()
 })()
