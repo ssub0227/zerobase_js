f47b8e8360772600949f6238b35949f38d54a574
ssub0227 <sb010227@gmail.com>
2023-11-17 02:11:27 +0900

    feat: 모달 보이기 toggle 기능 추가

--- modal_popup/practice/script.js
+++ modal_popup/practice/script.js
@@ -3,4 +3,17 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $button = get('.modal_open_button')
+  const $body = get('body')
+  const $modal = get('.modal')
+  const $modalCancleButton = get('.cancel')
+  const $modalConfirmButton = get('.confirm')
+
+  const toggleModal = () => {
+    $body.classList.toggle('scroll_lick')
+    $modal.classList.toggle('show')
+  }
+
+  $button.addEventListener('click', toggleModal)
 })()
