78d2ce89f498510d1729df7a0633228b4673ab44
ssub0227 <sb010227@gmail.com>
2023-11-21 21:01:57 +0900

    feat: 클릭 이벤트 구현

--- calculator/practice/script.js
+++ calculator/practice/script.js
@@ -8,4 +8,65 @@
   const getAll = (target) => {
     return document.querySelectorAll(target)
   }
+
+  class Calculator{
+    constructor (element){
+      this.element = element
+      this.currentValue = ''
+      this.prevValue = ''
+      this.operation = null
+    }
+
+    appendNumber(number){
+      if(number == '.' && this.currentValue.includes('.')) return
+      this.currentValue = this.currentValue.toString() + number.toString() // 숫자를 123 <- 123이라고 입력시 string 이 아니면 6이 됨
+    }
+
+    setOperation(operation){
+      this.resetOperation()
+      this.operation = operation
+      this.prevValue = this.currentValue
+      this.currentValue = ''
+
+      const elements = Array.from(getAll('.operation'))
+      const element = elements.filter((element) => element.innerText.includes(operation))[0]
+      element.classList.add('active')
+      
+    }
+
+    updateDisplay(){
+      if(this.currentValue){
+        this.element.value = this.currentValue
+        return
+      }
+    }
+
+    resetOperation(){
+      this.operation = null
+      const elements = Array.from(getAll('.operation'))
+      elements.forEach((element) => {
+        element.classList.remove('active')
+      })
+    }
+  }
+
+  const numberButtons = getAll('.cell_button.number')
+  const operationButtons = getAll('.cell_button.operation')
+  const display = get('.display')
+
+  const calculator = new Calculator(display)
+
+  numberButtons.forEach((button) => {
+    button.addEventListener('click', ()=>{
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+
+  operationButtons.forEach((button)=>{
+    button.addEventListener('click', ()=>{
+      calculator.setOperation(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
 })()
